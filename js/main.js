cb7fad76056a7e545efd5034a2c266bbc0078746
Rowan Hogan <rowan.hogan@gmail.com>
2016-03-02 22:19:06 -0500

    Update 2016-03-03T03:19:03.885Z

--- js/main.js
+++ js/main.js
@@ -279,11 +279,16 @@ var $ = require('jquery');
 require('slick-carousel');
 
 function handleMainPage() {
-  var imgSrc = $('#mp-tfa-img img').attr('src').replace('thumb/', ''),
-      arr = imgSrc.split('/');
+  var $img = $('#mp-tfa-img img');
 
-  arr.splice(arr.length - 1);
-  $('#mp-tfa-img').css({ 'background-image': 'url("' + arr.join('/') + '")' });
+  if ($img.length) {
+    var imgSrc = $('#mp-tfa-img img').attr('src').replace('thumb/', ''),
+        arr = imgSrc.split('/');
+
+    arr.splice(arr.length - 1);
+    $('#mp-tfa-img').css({ 'background-image': 'url("' + arr.join('/') + '")' });
+    $('#mp-tfa').addClass('has-bg-image');
+  }
 
   $('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick.min.css">');
 
