fb7fac4977616afba7cd6ad2e29ab86d405791a8
Rowan Hogan <rowan.hogan@gmail.com>
2016-05-07 22:24:33 -0400

    Small tweaks to main page styles

--- app/scripts/components/handleMainPage.js
+++ app/scripts/components/handleMainPage.js
@@ -1,6 +1,5 @@
 
 const $ = require('jquery');
-require('slick-carousel');
 
 function handleMainPage () {
   var $img = $('#mp-tfa-img img');
@@ -13,17 +12,6 @@ function handleMainPage () {
     $('#mp-tfa-img').css({ 'background-image' : 'url("' + arr.join('/') + '")' });
     $('#mp-tfa').addClass('has-bg-image');
   }
-
-  $('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick.min.css">');
-
-  $('#mp-dyk ul').slick({
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    dots: true,
-    fade: true
-  });
-
 }
 
 module.exports = handleMainPage;
