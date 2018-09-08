687e2e99206c7d9b82ef189c9e9de1671eeef7c2
Rowan Hogan <rowan.hogan@gmail.com>
2018-09-08 17:53:49 +1000

    Main page styling

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Page from '../../components/page'
 
 export default () => (
-  <div>
+  <div className="home-page">
     <div className="container">
-      <h1 className="logo">Wikipadia</h1>
+      <h1 className="page-title">Wikipadia</h1>
       <div className="blurb">
         <p>
           <em>WikiPadia</em> is a beautiful, customisable Wikipedia reader. It
           was specifically built for leisurely reading on an iPad.
+        </p>
+        <p>
+          <a href="https://github.com/rowanhogan/wikipadia">Source code</a>
         </p>
       </div>
     </div>
