6ae77f23b9110cd877bf6fa437d32b309b032229
Rowan Hogan <rowan.hogan@gmail.com>
2019-10-02 17:34:03 +1000

    Style fixes

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Page from '../../components/page'
+import React from "react";
+import Page from "../../components/page";
 
 export default () => (
   <div className="home-page">
@@ -7,16 +7,21 @@ export default () => (
       <h1 className="page-title">Wikipadia</h1>
       <div className="blurb">
         <p>
-          <em>Wikipadia</em> is a beautiful, minimal{' '}
+          <em>Wikipadia</em> is a beautiful, minimal{" "}
           <a href="http://www.wikipedia.org">Wikipedia</a> reader. It was
           specifically built for leisurely reading on an iPad.
         </p>
         <p>
-          <a href="https://github.com/rowanhogan/wikipadia">Source code</a>
+          <a
+            className="github-link"
+            href="https://github.com/rowanhogan/wikipadia"
+          >
+            Source code
+          </a>
         </p>
       </div>
     </div>
 
     <Page title="Main_page" />
   </div>
-)
+);
